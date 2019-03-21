391928f1d15a8ba7991ee5226fc225ba17c5277e
Asim Zaidi <asimzaidih@gmail.com>
2019-03-21 11:35:33 -0700

    painted pngs white, advantage section :sparkles:

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -26,8 +26,20 @@ class Home extends Component {
                         <img src='./assets/images/google.png'></img>
                     </div>
                 </div>
-                <div class='lower'>
-                    <h1>Woo waaaaa</h1>
+                <div className='advantage-block flexbox justify-content-space-evenly'>
+                    <div className='item'>
+                        <p className='advantage-block__title'>Over 100 restaurants</p>
+                        <p className='block__paragraph'>Take care of your menus easier than ever.</p>
+                    </div>
+                    <div className='item'>
+                        <p className='advantage-block__title'>Convenient and easy</p>
+                        <p className='block__paragraph'>Never login to another service to udpate the same things.</p>
+                    </div>
+
+                    <div className='item'>
+                        <p className='advantage-block__title'>Keep customers happy</p>
+                        <p className='block__paragraph'>Never again will customers see wrong prices listed.</p>
+                    </div>
                 </div>
             </section>
         );
