5404dcdecfa490537258a7ae217538dde0cce4ee
Asim Zaidi <asimzaidih@gmail.com>
2019-03-21 12:19:21 -0700

    basic footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
 import './styles/variables.scss'
 import './styles/components.scss';
 const App = () => (
@@ -13,6 +15,7 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route component={NoMatch} />
       </Switch>
+      <Footer />
     </div>
   </Router>
 );
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+const Footer = () => (
+  <footer className='footer'>
+        <p class='logo' className='advantage-block__title'>Digital Menu</p>
+
+        <p>Contact Us</p>
+        <p>Sign Up</p>
+        <p>Terms & Conditions</p>
+
+        <p>Digital Menu. 2019.</p>
+
+
+  </footer>
+);
+
+{/* <img className='logo' src='./assets/logo.png'></img> */}
+
+export default Footer;
