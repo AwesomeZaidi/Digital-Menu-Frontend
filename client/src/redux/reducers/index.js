3204ab9bd61b8ae6bc12d867a9eac8f6160adfa4
Asim Zaidi <asimzaidih@gmail.com>
2019-03-25 01:48:28 -0700

    rootReducer, initalState :zap:

---
+++ client/src/redux/reducers/index.js
@@ -0,0 +1,26 @@
+// src/js/reducers/index.js
+
+import { HANDLE_LOGIN, HANDLE_SIGNUP, HANDLE_ERROR, HANDLE_LOGOUT } from "../constants/action-types";
+
+const initialState = {
+  user: "",
+  error: false
+};
+
+function rootReducer(state = initialState, action) {
+  switch(action.type) {
+    case HANDLE_LOGIN:
+      return {...state, user: action.payload, error: action.payload_error}
+    case SIGNUP_USER:
+      return {...state, user: action.payload, error: action.payload_error}
+    case HANDLE_ERROR:
+      return {...state, error: action.payload}
+    case HANDLE_LOGOUT:
+      return {...state, user: "", error: false}
+    default: 
+        return state;
+  }
+};
+
+
+export default rootReducer;
