70513657de8169aa425d629e3e091338f364c2e9
Asim Zaidi <asimzaidih@gmail.com>
2019-03-25 01:37:24 -0700

    added jsx back into home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,22 +1,43 @@
 import React, { Component} from 'react';
 import HowItWorks from '../../components/HowItWorks';
-import Advantage from '../../components/Advantage';
-
+import { Redirect } from 'react-router';
 import "../../styles/home.scss";
 
 class Home extends Component {
-    // 🤔 probably don't need a state here.
+    constructor(props) {
+        super(props);
+        this.state = {
+            page: null
+        };
+    };
+
+    handleRedirect(page) {
+        this.setState({ page: page });
+    };
+
     render() {
+
+
+        // not sure why this doesn't work.
+        // console.log(this.state.page === 'signup');
+        
+        // if (this.state.page) {
+        //     <Redirect to={this.state.page}/>  
+        // }
+        
         return (
             <section className='home'>
+                
+                {/* TOP */}
                 <div className='top'>
                     <div className='left-content section-side-padding'>
-                        <h1 className='__title'>Manage menus</h1>
+                        <h1 className='block__title'>Manage menus</h1>
 
-                        <p className='__paragraph'>Update the prices for all your online menues using our intuitive menu manager.</p>
+                        <p className='block__paragraph'>Update the prices for all your online menues using our intuitive menu manager.</p>
 
-                        <a className='__btn'>Sign up</a>
+                        <a onClick={() => window.location.href = '/signup'} className='block__btn'>Sign up</a>
                     </div>
+                    
                     <div className='lower-logos align-items-center flexbox justify-content-center section-side-padding'>
                         <img src='./assets/images/doordash.png'></img>
                         <img src='./assets/images/grubhub.png'></img>
@@ -27,8 +48,26 @@ class Home extends Component {
                         <img src='./assets/images/google.png'></img>
                     </div>
                 </div>
-                <Advantage />
+
+                {/* ADVANTAGE */}
+                <div className='advantage-block flexbox justify-content-space-evenly section-padding'>
+                    <div className='item'>
+                        <p className='advantage-block__title'>Over 100 restaurants</p>
+                        <p className='block__paragraph'>Take care of your menus easier than ever.</p>
+                    </div>
+                    <div className='item'>
+                        <p className='advantage-block__title'>Convenient and easy</p>
+                        <p className='block__paragraph'>Never login to another service to udpate the same things.</p>
+                    </div>
+                    <div className='item'>
+                        <p className='advantage-block__title'>Keep customers happy</p>
+                        <p className='block__paragraph'>Never again will customers see wrong prices listed.</p>
+                    </div>
+                </div>
+
+                {/* HOW IT WORKS */}
                 <HowItWorks />
+
             </section>
         );
     }
