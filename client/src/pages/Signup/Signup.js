6b0145b49be9b3d441e827e066f859178110e0fd
Asim Zaidi <asimzaidih@gmail.com>
2019-03-25 01:50:46 -0700

    path

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Redirect } from 'react-router';
-import { signup } from "../../js/actions/index";
-import logo from '../../Images/logo-only.png';
+import { signup } from '../../redux/actions/index';
+// import logo from '../../Images/logo-only.png';
 
 class Signup extends Component { 
     constructor(props) {
