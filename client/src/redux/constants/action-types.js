6d1b28163fd5bfaffdc0e99929d0a7365a6d6645
Asim Zaidi <asimzaidih@gmail.com>
2019-03-25 01:45:10 -0700

    action types created :zap:

---
+++ client/src/redux/constants/action-types.js
@@ -0,0 +1,9 @@
+// src/js/constants/action-types.js
+
+export const GET_ERRORS = "GET_ERRORS";
+export const USER_LOADING = "USER_LOADING";
+export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const HANDLE_LOGIN = "HANDLE_LOGIN";
+export const HANDLE_SIGNUP = "SIGNUP_USER";
+export const LOGOUT_USER = "LOGOUT_USER";
+export const HANDLE_ERROR = "HANDLE_ERROR";
