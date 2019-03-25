cbf95a53b11e2dac6bda4194746a57bd805ef7f9
Asim Zaidi <asimzaidih@gmail.com>
2019-03-25 02:26:34 -0700

    wrapped App in Provided store

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render((
+    <Provider store={store}>
+        <App />
+    </Provider>
+), document.getElementById('root'));
 serviceWorker.unregister();
