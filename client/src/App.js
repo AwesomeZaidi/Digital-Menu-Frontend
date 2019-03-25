e51a7f5ea8e80e738218dcec85eaa548cbb15346
Asim Zaidi <asimzaidih@gmail.com>
2019-03-25 01:36:26 -0700

    added signup path

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";  
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-
 import './styles/variables.scss'
 import './styles/components.scss';
+
 const App = () => (
   <Router>
     <div>
       <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={Signup} />
         <Route component={NoMatch} />
       </Switch>
       <Footer />
