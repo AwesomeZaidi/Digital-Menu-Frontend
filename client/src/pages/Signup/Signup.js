b97845c5e0222f15f4b685a805a85ea6ad10c3cd
Asim Zaidi <asimzaidih@gmail.com>
2019-03-25 18:24:28 -0700

    Signup view styled

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -35,52 +35,66 @@ class Signup extends Component {
             return <Redirect to='/dashboard' />
         }
         return (
-            <section className='signup'>
-            <h1>Join Digital Menu now</h1> 
-            
-            <form onSubmit={this.handleSubmit}>
-                <legend>Sign up</legend>
-                 <p>Now’s your chance to get your free digital menu. There’s no better way to connect all your digital menus,
-                    never have wrong listed prices again, and bring in more business.
-                 </p>
+            <div>
+                <div class='hero-section'>
+                    <h1 class='center'>Join the Pilot Program now</h1>
+                </div>
+                <section className='signup form-wrapper'>                
+                    <div class='user-form' onSubmit={this.handleSubmit}>
+                        <h2 class='content-section'>Sign up. Manage your menus.<br></br>Avoid issues.</h2>
+                        <p class='content-section'>Now’s your chance to get your free digital menu. There’s no better way to connect all your digital menus,
+                            never have wrong listed prices again, and bring in more business.
+                        </p>
 
-                 <h2>So what happens when you submit this form?</h2>
-                 <ul>
-                     <li>We’ll reach out to let you know if you’ve been selected.</li>
-                     <li>If you get picked, we’ll collect more info to get things set up for you.</li>
-                     <li>We’ll keep you posted about the program’s timeline and other details.</li>
-                 </ul>
-                 
-                 <p>Even if you don’t get picked, you can still become an early adopter. You’ll be eligible for huge discounts and exclusive offers.</p>
+                        <h4 class='content-section'>So what happens when you submit this form?</h4>
+                        <ul class='content-section'>
+                            <li class='content-section'>We’ll reach out to let you know if you’ve been selected.</li>
+                            <li class='content-section'>If you get picked, we’ll collect more info to get things set up for you.</li>
+                            <li class='content-section'>We’ll keep you posted about the program’s timeline and other details.</li>
+                        </ul>
+                        
+                        <p class='content-medium'>Even if you don’t get picked, you can still become an early adopter. You’ll be eligible for huge discounts and exclusive offers.</p>
 
-                 <p>When we say free, we mean it — no signup fees, upfront costs, or anything like that. You really have nothing to lose.
-                     Join now for your shot at all the benefits Anytime Link has to offer.
-                 </p>
-                
-                 <h2>Personal information</h2>
-                 <p>Tell us how to contact you. We respect your privacy and we’ll never sell, rent, lease or give away your information — ever.</p>
+                        <p class='content-medium'>When we say free, we mean it — <strong>no signup fees, upfront costs, or anything like that.</strong> You really have nothing to lose.
+                            Join now for your shot at all the benefits Anytime Link has to offer.
+                        </p>
+                            <div class='push-down'></div>
+                            <div class="section-divider"></div>
+                            <div class='pad-down'></div>
+                            <h4 class='content-section'>Personal information</h4>
+                            <p class='content-section'>Tell us how to contact you. We respect your privacy and we’ll never sell, rent, lease or give away your information — ever.</p>
 
-                 <label for='email'>Your email</label>
-                 <input name='email' placeholder='name@domain.com'></input>
+                            <form class='top-down-left'>
+                                <div class='form-group'>
+                                    <label for='email'>Your email</label>
+                                    <input name='email' placeholder='name@domain.com'></input>
+                                </div>
+                                
+                                <div class='form-group'>
+                                    <label for='fullname'>Your full name</label>
+                                    <input name='fullname' placeholder='Carmen Caliente'></input>
+                                </div>
+                                <div class='form-group'>
+                                    <label for='position'>Your position</label>
+                                    <input name='position' placeholder='Position'></input>
+                                </div>
+                                <div class='form-group'>
+                                    <label for='number'>Your contact phone</label>
+                                    <input name='number' placeholder='(123)-345-6789'></input>
+                                </div>
 
-                 <label for='fullname'>Your full name</label>
-                 <input name='fullname' placeholder='Carmen Caliente'></input>
+                                <p>We may contact you between 11 AM and 5 PM Eastern</p>
 
-                 <label for='position'>Your position</label>
-                 <input name='position' placeholder='Position'></input>
-                    
-                 <label for='number'>Your contact phone</label>
-                 <input name='number' placeholder='(123)-345-6789'></input>
-                 <p>We may contact you between 11 AM and 5 PM Eastern</p>
+                                {/* <h2>Restaurant information</h2>
+                                <p>Tell us about your restaurant. No matter where you are we can make your menu digital and better.</p> */}
 
-                 {/* <h2>Restaurant information</h2>
-                 <p>Tell us about your restaurant. No matter where you are we can make your menu digital and better.</p> */}
-
-                 {/* <label for='number'>Your contact phone</label>
-                 <input name='number' placeholder='(123)-345-6789'></input> */}
-                 <button type='submit'>Join Digital Menu</button>
-            </form>
-     </section>
+                                {/* <label for='number'>Your contact phone</label>
+                                <input name='number' placeholder='(123)-345-6789'></input> */}
+                                <button type='submit'>Join Digital Menu</button>
+                            </form>
+                    </div>
+                </section>
+            </div>
         );
     }
 }
