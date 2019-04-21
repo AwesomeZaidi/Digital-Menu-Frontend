e4a11af5ae605309b00fe8aed2e9825e4bc87879
Asim Zaidi <asimzaidih@gmail.com>
2019-04-20 18:53:23 -0700

    edit btn

--- src/components/Buttons/Edit.js
+++ src/components/Buttons/Edit.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default class Edit extends Component {
     render() {
+        const locationSpecificLink = `/location/${this.props.locationId}`;
         return (
-            <button>Edit</button>
+            <Link to={locationSpecificLink}>Edit {this.props.title}</Link>
         );
-    }
-}
+    };
+};
