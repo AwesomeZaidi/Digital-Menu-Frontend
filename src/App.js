a49c7bd346a49732c3f60e39395dfe13f3885109
Asim Zaidi <asimzaidih@gmail.com>
2019-05-29 14:38:38 -0500

    [Changed] app pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Signup from "./pages/Signup";
 import NoMatch from "./pages/NoMatch";
 import AddRestaurant from "./pages/AddRestaurant";
 import Items from "./pages/Items";
+import ManageLocation from "./pages/ManageLocation";
 import Locations from "./pages/Locations";
 import NavBar from "./components/NavBar";
 import SideMenu from "./components/SideMenu";
@@ -21,8 +22,9 @@ const App = () => (
         <Route exact path="/" component={Home} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/addRestaurant" component={AddRestaurant} />
-        <Route exact path="/locations" component={Locations} />
+        <Route exact path="/restaurant/:restaurantId/locations" component={Locations} />
         <Route exact path="/addLocation" component={AddLocation} />
+        <Route exact path="/restaurant/:restaurantId/location/:locationId/" component={ManageLocation} />        
         <Route exact path="/restaurant/:restaurantId/location/:locationId/items" component={Items} />        
         <Route component={NoMatch} />
       </Switch>
