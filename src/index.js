60bf1b0471070215e54b179bd3cb30da32d5aeeb
Asim Zaidi <asimzaidih@gmail.com>
2019-06-07 18:43:48 -0700

    changed cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
-axios.defaults.withCredentials = true  // enable axios post cookie, default false
+axios.defaults.withCredentials = false  // enable axios post cookie, default false
 ReactDOM.render((
     <Provider store={store}>
         <App />
